44967b77300307c1ab375b39e7b3760707f17725
Martin Hansen <android41cv@gmail.com>
2011-03-14 20:21:18 +0000

    Notes about a more efficient ListView for the TableViewer

--- src/dk/andsen/asqlitemanager/TableViewer.java
+++ src/dk/andsen/asqlitemanager/TableViewer.java
@@ -177,6 +177,7 @@ public class TableViewer extends Activity implements OnClickListener {
 			// Adding all columns as TextView's should be changed to a ConvertView
 			// as described here:
 			// http://android-er.blogspot.com/2010/06/using-convertview-in-getview-to-make.html
+			// or in android41cv dk.andsen.utils.MyArrayAdapter
 			for(int j=0; j<colSize; j++){
 				TextView c = new TextView(this);
 				c.setText(data[i][j]);
