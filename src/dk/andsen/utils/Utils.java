06f59b8a5cf1a622d4394494735c02745a397c7a
Martin Hansen <android41cv@gmail.com>
2011-11-17 20:54:00 +0000

    Small fix

--- src/dk/andsen/utils/Utils.java
+++ src/dk/andsen/utils/Utils.java
@@ -15,7 +15,7 @@ import android.view.Gravity;
 import android.widget.Toast;
 
 public class Utils {
-	private static boolean logging = true;
+	public static boolean logging = true;
 	
 	private static String app = "aSQLMan";
 	/**
